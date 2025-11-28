687f9f3aa9f04c18154065352a0f27b351b62061
Ricardoassousa <ricardoassousa@hotmail.com>
2025-11-27 23:30:27 -0100

    test: homepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,7 +14,7 @@ class DefaultController extends AbstractController
     public function index(): Response
     {
         return $this->render('index.html.twig', [
-            'message' => 'Olá, Symfony 5!',
+            'message' => 'Home',
         ]);
     }
 }
