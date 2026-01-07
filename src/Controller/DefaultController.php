926b79af5c5777e6289a6dcee2b4974f6459675c
Ricardoassousa <ricardoassousa@hotmail.com>
2026-01-06 23:00:14 -0100

    chore: update homepage nav bar route & template and add /catalog & /cart routes for ROLE_USER

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,15 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * DefaultController is used to manage basic routes and render views.
+ * It handles the homepage and potentially other default pages of the application.
+ */
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * Displays the homepage.
+     *
+     * This action renders the homepage template and passes a message to the view.
+     * The message is displayed on the homepage and can be customized.
+     *
+     * @return Response
      */
-    public function index(): Response
+    public function homepageAction(): Response
     {
-        return $this->render('index.html.twig', [
-            'message' => 'Home',
-        ]);
+        return $this->render('homepage.html.twig', []);
     }
+
 }
